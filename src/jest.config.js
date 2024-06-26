4fc69d3c113c86c1dd790c95abdf1c0437ff0146
Zufar Murodov <zufar4554murodov@gmail.com>
2024-06-26 12:32:12 +0500

    test running

---
+++ src/jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  testMatch: ['**/?(*.)+(test).[jt]s?(x)'], // Adjust to match your test file naming convention
+  testPathIgnorePatterns: ['/node_modules/', '/e2e_tests/'], // Add any other directories to ignore
+};
